a7a008b57600411bcd7e6a463e7903c2788d2702
Aritro Chowdhury <chowdhury.aritro1998@gmail.com>
2023-06-28 03:02:52 +0530

    Configured permissions and token authentication for UserProfileViewSet

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,9 +4,13 @@ from rest_framework.response import Response
 from .serializers import UserProfileSerializer
 from . import models
 from rest_framework import serializers, viewsets
+from rest_framework.authentication import TokenAuthentication
+from . import permissions
 # Create your views here.
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     # Handle creating and updating profiles
     serializer_class = UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
