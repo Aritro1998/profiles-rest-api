dec67eabe8ea071cb3406956a56a561dae62a84f
Aritro Chowdhury <chowdhury.aritro1998@gmail.com>
2023-06-28 02:47:35 +0530

    Add serializer and viewsets for model UserProfile

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserProfileSerializer
+from . import models
+from rest_framework import serializers, viewsets
 # Create your views here.
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    # Handle creating and updating profiles
+    serializer_class = UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
