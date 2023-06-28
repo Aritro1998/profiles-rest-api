c9c2dc0263a775c59f14d0d7803e94e594dc99d3
Aritro Chowdhury <chowdhury.aritro1998@gmail.com>
2023-06-28 19:22:42 +0530

    Added users profile feed api

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Profiles REST API code
 #### Setup commands
 - vagrant init ubuntu/bionic64
 - vagrant up
+- vagrant ssh
+- cd /vagrant/
 - python -m venv ~/env
 - source ~/env/bin/activate
 - pip install -r requirements.txt
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import UserProfileSerializer
+from .serializers import UserProfileSerializer, ProfileFeedItemSerializer
 from . import models
 from rest_framework import filters, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from . import permissions
 from rest_framework.settings import api_settings
 # Create your views here.
@@ -23,3 +24,18 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 class UserLoginApiView(ObtainAuthToken):
     # Handle creating user authentication tokens
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class UserProfileFeedViewSet(viewsets.ModelViewSet):
+    # Handles creating, reading and updating profile feed items
+    authentication_classes = (TokenAuthentication,)
+    serializer_class = ProfileFeedItemSerializer
+    queryset = models.ProfileFeedItem.objects.all()
+    permission_classes = (
+        permissions.UpdateOwnStatus,
+        IsAuthenticatedOrReadOnly,
+    )
+
+    def perform_create(self, serializer):
+        # Sets the user profile to the logged in user
+        serializer.save(user_profile=self.request.user)
